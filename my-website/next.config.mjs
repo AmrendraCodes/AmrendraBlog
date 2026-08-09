cb98e1f405da8442091c4a045f29aa83290df27a
AmrendraCodes <amrendra1999official@gmail.com>
2026-08-10 00:19:22 +0530

    fix(csp): update Content-Security-Policy to allow connect.facebook.net for Meta Pixel execution

--- my-website/next.config.mjs
+++ my-website/next.config.mjs
@@ -95,7 +95,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https:; frame-ancestors 'none';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net https://www.facebook.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https: https://www.facebook.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https: https://www.facebook.com https://connect.facebook.net; frame-ancestors 'none';",
           },
         ],
       },
